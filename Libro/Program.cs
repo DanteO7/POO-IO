c6045d7bca9373c951cd58fd09a99c3e065e22e0
Dante07 <dante.orsetti@gmail.com>
2024-08-07 22:47:05 -0300

    Clases Programacion 2

---
+++ Libro/Program.cs
@@ -0,0 +1,41 @@
+﻿class Libro
+{
+    public string Titulo {  get; set; }
+    public string Autor {  get; set; }
+    public string Genero { get; set; }
+    public int NumeroDePaginas { get; set; }
+    public Libro(string titulo, string autor, string genero, int numeroDePaginas)
+    {
+        Titulo = titulo;
+        Autor = autor;
+        Genero = genero;
+        NumeroDePaginas = numeroDePaginas;
+    }
+    public void MostrarInformacionLibro()
+    {
+        Console.WriteLine($"\nTitulo: {Titulo}\nAutor: {Autor}\nGenero: {Genero}\nNumero de paginas: {NumeroDePaginas}\n ");
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        string titulo, autor, genero;
+        int numeroDePaginas;
+
+        Console.Write("Ingrese el titulo del libro: ");
+        titulo = Console.ReadLine();
+
+        Console.Write("Ingrese el autor del libro: ");
+        autor = Console.ReadLine();
+
+        Console.Write("Ingrese el genero del libro: ");
+        genero = Console.ReadLine();
+
+        Console.Write("Ingrese el numero de paginas del libro: ");
+        int.TryParse(Console.ReadLine(), out numeroDePaginas);
+
+        Libro libro = new Libro(titulo, autor, genero, numeroDePaginas);
+        libro.MostrarInformacionLibro();
+    }
+}
