4c3aba558596554f675c2da79f2a4872810c5ad6
Orsetti Martinez <dorsettimartinez@FRSN.UTN.EDU.AR>
2024-08-13 18:09:37 -0300

    Clases Programacion 2

---
+++ Supermercado/Program.cs
@@ -0,0 +1,56 @@
+﻿class Producto
+{
+    public string Codigo { get; private set; }
+    public string Nombre { get; private set; }
+    public int CantidadStock { get; private set; }
+    public decimal Precio { get; private set; }
+    public Producto(string codigo, string nombre, int cantidadStock, decimal precio)
+    {
+        Codigo = codigo;
+        Nombre = nombre;
+        CantidadStock = cantidadStock;
+        Precio = precio;
+    }
+    public void ActualizarStock(int cantidad)
+    {
+        if (cantidad<=CantidadStock)
+        {
+            CantidadStock += cantidad;
+        }
+        else
+        {
+            Console.WriteLine("No se puede");
+        }
+    }
+}
+class OrdenCompra
+{
+    private List<Producto> _productos;
+    public OrdenCompra()
+    {
+        _productos = new List<Producto>();
+    }
+    public void AgregarProducto(Producto producto)
+    {
+        _productos.Add(producto);
+    }
+    public decimal CalcularTotal()
+    {
+        decimal total = 0;
+        foreach (var producto in _productos)
+        {
+            total += producto.Precio;
+        }
+        return total;
+    }
+    public void MostrarDetalleOrden()
+    {
+        Console.WriteLine("Detalle de la orden de compra: ");
+        Console.WriteLine($"Total de la orden: {CalcularTotal():C}");
+        foreach(var producto in _productos)
+        {
+            Console.WriteLine($"Producto: {producto.Nombre}, Precio: {producto.Precio}");
+        }
+
+    }
+}
