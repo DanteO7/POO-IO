04fe8233e4e8b1aa63006813ba239467da4d0bd3
DanteO7 <166159559+DanteO7@users.noreply.github.com>
2024-08-22 15:59:34 -0300

    Program.cs

--- Hospital/Program.cs
+++ Hospital/Program.cs
@@ -1,7 +1,40 @@
-﻿class Program
+namespace Hospital.Modelos
 {
-    static void Main()
+    class Program
     {
+        static void Main()
+        {
+            Especializacion especializacion = new Especializacion("Cardiología");
 
+            Hospital hospital = new Hospital("Hospital Central", "Calle Falsa 123");
+
+            Departamento cardiologia = new Departamento("Cardiología");
+
+            Medico drHouse = new Medico("Dr. House", especializacion);
+
+            Paciente paciente = new Paciente("Juan Perez", 30);
+
+            Procedimiento procedimiento = new Procedimiento("Cirugía de corazón");
+
+            EquipoMedico equipoMedico = new EquipoMedico("Visturi", "Cortante");
+
+            SalaDeOperaciones sala = new SalaDeOperaciones(1);
+
+            hospital.AgregarDepa(cardiologia);
+
+            cardiologia.AgregarMedico(drHouse);
+
+            drHouse.AtenderPaciente(paciente);
+
+            cardiologia.AgregarProcedimiento(procedimiento);
+
+            procedimiento.AsignarSala(sala);
+
+            var pacientes = drHouse.ObtenerPacientes();
+            foreach (var item in pacientes)
+            {
+                Console.WriteLine($"{item.Nombre}, {item.Edad}");
+            }
+        }
     }
 }
